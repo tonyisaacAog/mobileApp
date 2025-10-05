ad0110148e38d5ee879616b17ae6840dca41bd09
tonyisaacG <antonyisaacgayed@gmail.com>
2025-10-05 17:47:37 +0300

    clear

--- obj/Debug/net8.0/CompanyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CompanyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a04d33564c2f264d4664f5550f175081b2df58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c25a47d7a0abab0f9b2defda817e85e4e18199")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
